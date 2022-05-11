77a09740a1393484a3047a0fa7acd7ed05de2be0
akmalrafidiara <akmalrafidiara@gmail.com>
2022-05-11 16:15:37 +0700

    Bubelsort dinamis

--- Queue/Readme.md
+++ Queue/Readme.md
@@ -3,8 +3,9 @@ Struktur Data dan Algoritma
 
 No | Nama                    | Materi
 ---------------------------------------
-1  | Rawdo Madina            | Queue 1
-2  | Salwa Tsabitah          | Queue 2
-3  | Raden Roro Ziva A       | Queue 3
-4  | M Aqmal Khafidz Pratama | Queue 4
-5  | Akmal Rafi Diara Putra  | Queue 1 Menggunakan Objek
+1  | Rawdo Madina            | Linkedlist Reverse String
+2  | Salwa Tsabitah          | Linkedlist Stack/Tumpukan
+3  | Raden Roro Ziva A       | Linkedlist Queue/Antrian
+4  | M Aqmal Khafidz Pratama | Linkedlist Sort
+5  | Akmal Rafi Diara Putra  | Linkedlist Ganda
+6  | M Aqmal Khafidz Pratama | Linkedlist Postfix
--- Sort/ke-1/tugas1_bubble_sort.py
+++ Sort/ke-1/tugas1_bubble_sort.py
@@ -1,3 +1,4 @@
+import time
 # Optimized Bubble sort in Python
 def bubbleSort(arr):
   n = len(arr)
@@ -5,43 +6,99 @@ def bubbleSort(arr):
     for j in range(0, n - i - 1):
       if arr[j] > arr[j + 1] :
         arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        time.sleep(0.2)
+        print(arr)
   return arr
 
+def rev_bubbleSort(arr):
+  n = len(arr)
+  for i in range(n):
+    for j in range(0, n - i - 1):
+      if arr[j] < arr[j + 1] :
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        time.sleep(0.2)
+        print(arr)
+  return arr
+
+def pilihAksi():
+  print("Daftar Aksi:\n 1. Mengurutkan Keatas\n 2. Mengurutkan Kebawah\n 3. Keluar")
+  pilihan = input("Masukkan pilihan Anda (1, 2, atau 3): ")
+  return pilihan
+
 def main():
-  ingin_lanjut = True
-  while ingin_lanjut:
-    print('------ Program Bubble Sort ------')
-    print('* CATATAN *')
-    print('Mohon masukkan angka dengan tanda koma sebagai pemisah. Jika dirasa sudah cukup, maka tekan enter untuk melakukan proses pengurutan angka.')
-    print('Contoh memasukkan angka : 21, 32, 17, 2, 51')
+  print('------ Program Bubble Sort ------')
+  print('* CATATAN *')
+  print('Mohon masukkan angka dengan tanda koma sebagai pemisah.\nJika dirasa sudah cukup, maka tekan enter untuk melakukan proses pengurutan angka.')
+  print('Contoh memasukkan angka : 21, 32, 17, 2, 51')
+  state_try = True
+  while state_try:
     angka = input('Masukkan beberapa angka acak yang akan diurutkan: ')
     data = angka.split(", ")
     new_data = []
     for i in range(len(data)):
       new_data.append(int(data[i]))
-
-    bubbleSort(new_data)
-    print('Hasil: ')
-    for i in range(len(new_data)):
-      if i != len(new_data) - 1:
-        print(new_data[i], end=", ")
+    clear = True
+    while clear:
+      aksi = pilihAksi()
+      if aksi == "1":
+        bubbleSort(new_data)
+        print('Hasil pengurutan keatas: ')
+        for i in range(len(new_data)):
+          if i != len(new_data) - 1:
+            print(new_data[i], end=", ")
+          else:
+            print(new_data[i])
+      elif aksi == "2":
+        rev_bubbleSort(new_data)
+        print('Hasil pengurutan kebawah: ')
+        for i in range(len(new_data)):
+          if i != len(new_data) - 1:
+            print(new_data[i], end=", ")
+          else:
+            print(new_data[i])
+      elif aksi == "3":
+        clear = False
       else:
-        print(new_data[i])
+        print("Pilihan tidak valid\n")
+    try_again = input("Ingin menjalankan program kembali? (Y/N): ").upper
+    if try_again == "N":
+      state_try = False
 
-    print('\n--- Pilihan ---')
-    print('1. Program akan dijalankan kembali')
-    print('2. Program akan diakhiri')
-    konfirmasi = True
-    while konfirmasi:
-      konfirmasi = input("Apakah Anda ingin menjalankan program ini kembali? (ketik 1 atau 2): ")
-      if konfirmasi == '1':
-        print("Baik, program dijalankan kembali\n")
-        konfirmasi = False
-      elif konfirmasi == '2':
-        print('Program diakhiri. Sekian, terima kasih\n')
-        konfirmasi = ingin_lanjut = False
-      else:
-        print('* Peringatan *')
-        print(f'Mohon maaf, pilihan {konfirmasi} tidak tersedia')
-        print('Mohon ketik dengan pilihan yang tersedia\n')
+# def main():
+#   ingin_lanjut = True
+#   while ingin_lanjut:
+#     print('------ Program Bubble Sort ------')
+#     print('* CATATAN *')
+#     print('Mohon masukkan angka dengan tanda koma sebagai pemisah. Jika dirasa sudah cukup, maka tekan enter untuk melakukan proses pengurutan angka.')
+#     print('Contoh memasukkan angka : 21, 32, 17, 2, 51')
+#     angka = input('Masukkan beberapa angka acak yang akan diurutkan: ')
+#     data = angka.split(", ")
+#     new_data = []
+#     for i in range(len(data)):
+#       new_data.append(int(data[i]))
+
+#     bubbleSort(new_data)
+#     print('Hasil: ')
+#     for i in range(len(new_data)):
+#       if i != len(new_data) - 1:
+#         print(new_data[i], end=", ")
+#       else:
+#         print(new_data[i])
+
+#     print('\n--- Pilihan ---')
+#     print('1. Program akan dijalankan kembali')
+#     print('2. Program akan diakhiri')
+#     konfirmasi = True
+#     while konfirmasi:
+#       konfirmasi = input("Apakah Anda ingin menjalankan program ini kembali? (ketik 1 atau 2): ")
+#       if konfirmasi == '1':
+#         print("Baik, program dijalankan kembali\n")
+#         konfirmasi = False
+#       elif konfirmasi == '2':
+#         print('Program diakhiri. Sekian, terima kasih\n')
+#         konfirmasi = ingin_lanjut = False
+#       else:
+#         print('* Peringatan *')
+#         print(f'Mohon maaf, pilihan {konfirmasi} tidak tersedia')
+#         print('Mohon ketik dengan pilihan yang tersedia\n')
 main()

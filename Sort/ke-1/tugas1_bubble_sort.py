bcb91f5012af0faf642a7cae8cee3793358a7ded
aqmalpratama <aqmal.pratama81@gmail.com>
2022-05-16 09:42:08 +0700

    Memperbaiki bubble sort

--- Sort/ke-1/tugas1_bubble_sort.py
+++ Sort/ke-1/tugas1_bubble_sort.py
@@ -5,9 +5,12 @@ def bubbleSort(arr):
   for i in range(n):
     for j in range(0, n - i - 1):
       if arr[j] > arr[j + 1] :
-        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+
         time.sleep(0.2)
-        print(arr)
+        print(f'Adanya penukaran antara {arr[j]} dengan {arr[j + 1]}, urutan data menjadi {arr}')
   return arr
 
 def rev_bubbleSort(arr):
@@ -15,13 +18,16 @@ def rev_bubbleSort(arr):
   for i in range(n):
     for j in range(0, n - i - 1):
       if arr[j] < arr[j + 1] :
-        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+
         time.sleep(0.2)
-        print(arr)
+        print(f'Adanya penukaran antara {arr[j]} dengan {arr[j + 1]}, urutan data menjadi {arr}')
   return arr
 
 def pilihAksi():
-  print("Daftar Aksi:\n 1. Mengurutkan Keatas\n 2. Mengurutkan Kebawah\n 3. Keluar")
+  print("\nDaftar Aksi:\n 1. Mengurutkan Keatas\n 2. Mengurutkan Kebawah\n 3. Keluar")
   pilihan = input("Masukkan pilihan Anda (1, 2, atau 3): ")
   return pilihan
 
@@ -29,7 +35,7 @@ def main():
   print('------ Program Bubble Sort ------')
   print('* CATATAN *')
   print('Mohon masukkan angka dengan tanda koma sebagai pemisah.\nJika dirasa sudah cukup, maka tekan enter untuk melakukan proses pengurutan angka.')
-  print('Contoh memasukkan angka : 21, 32, 17, 2, 51')
+  print('Contoh memasukkan angka : 25, 57, 48, 39, 18, 95, 80, 35')
   state_try = True
   while state_try:
     angka = input('Masukkan beberapa angka acak yang akan diurutkan: ')
@@ -37,17 +43,19 @@ def main():
     new_data = []
     for i in range(len(data)):
       new_data.append(int(data[i]))
+
     clear = True
     while clear:
       aksi = pilihAksi()
       if aksi == "1":
         bubbleSort(new_data)
-        print('Hasil pengurutan keatas: ')
+        print('\nHasil pengurutan keatas: ')
         for i in range(len(new_data)):
           if i != len(new_data) - 1:
             print(new_data[i], end=", ")
           else:
             print(new_data[i])
+      
       elif aksi == "2":
         rev_bubbleSort(new_data)
         print('Hasil pengurutan kebawah: ')
@@ -56,8 +64,10 @@ def main():
             print(new_data[i], end=", ")
           else:
             print(new_data[i])
+      
       elif aksi == "3":
         clear = False
+      
       else:
         print("Pilihan tidak valid\n")
     try_again = input("Ingin menjalankan program kembali? (Y/N): ")
